80c26ae91f3c3d310d208fdac7fd897c9f8cd751
FilsAime, Jonathan (plp2675) <jonathan.filsaime@usaa.com>
2019-10-18 20:22:23 -0500

    final version

--- src/Main.java
+++ src/Main.java
@@ -4,30 +4,29 @@ public class Main {
 
     public static void main(String[] args) {
 
-
         while (true) {
+
             System.out.print("Enter an operation: ");
 
-            Calculator calculator = Calculator.getInstance();
-
+            //collect user input
             Scanner scanner = new Scanner(System.in);
-
             String readThisString = scanner.nextLine();
-            System.out.println(readThisString);
-
             readThisString = readThisString.concat("\n");
             char[] a = readThisString.toCharArray();
 
+            Calculator calculator = Calculator.getInstance();
 
+            //The state machine starts from state zero
             for (char c : a) {
                 calculator.getCurrentState().process(c, calculator);
             }
 
+            //If there are no errors print the total
             if(calculator.getL() != 'E'){
                 System.out.println("Total = " + calculator.getT());
             }
 
-
+            //reset state and start over
             calculator.setCurrentState(StateRestart.getInstance());
             calculator.getCurrentState().process(' ', calculator);
         }
